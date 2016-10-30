b3adb059836ce989ff71dfc105a61396868a7c1a
Anton <tonnyphys@gmail.com>
2016-10-30 19:17:06 +0200

    Made hometask #3

---
+++ app/Time.php
@@ -0,0 +1,17 @@
+<?php
+namespace Venom\Batman;
+
+/**
+ * Created by PhpStorm.
+ * User: tonny
+ * Date: 30.10.16
+ * Time: 16:12
+ */
+class index
+{
+    public function index()
+    {
+        echo "sfsdf0";
+        var_dump('aaaaa');
+    }
+}
---
+++ app/Users/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tonny
+ * Date: 30.10.16
+ * Time: 19:01
+ */
+
+namespace Venom\Batman\Users;
+
+
+class User
+{
+
+}
